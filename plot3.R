bd3e759d93cc3285468258429bab87745a2eda79
jeffalltogether <jeff.thatcher@gmail.com>
2015-07-12 17:21:28 -0500

    more updates

--- plot3.R
+++ plot3.R
@@ -29,6 +29,6 @@ with(data, {
         lines(Times, Sub_metering_2, type = "l", col = "red")
         lines(Times, Sub_metering_3, type = "l", col = "blue")
         legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), 
-               lty=c(1,1), lwd=c(2.5,2.5),col =c("black","red","blue"))
+               lty=c(1,1), lwd=c(2.5,2.5),col =c("black","red","blue"), cex = 1)
         })
 
